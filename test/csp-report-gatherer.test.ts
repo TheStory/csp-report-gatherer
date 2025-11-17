e14eb443923f297d36883e3ecf528655d9763295
Yaroslav Shatkevich <jarek@thestory.pl>
2025-11-17 10:39:25 +0100

    Initial commit

---
+++ test/csp-report-gatherer.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CspReportGatherer from '../lib/csp-report-gatherer-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/csp-report-gatherer-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CspReportGatherer.CspReportGathererStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
